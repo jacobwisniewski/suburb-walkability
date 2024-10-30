556476ae7b1c02f69f18556f96abb8a014875403
Jacob Wisniewski <jacob.wisniewski@rea-group.com>
2024-10-30 13:17:25 +1100

    improving dockerization

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,14 +9,17 @@ import { LocationType } from "./entity/LocationType";
 import { Coles } from "./entity/Coles";
 import { Aldi } from "./entity/Aldi";
 import { PropertyPrice } from "./entity/PropertyPrice";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "jacob.wisniewski",
-  password: "",
-  database: "geo_db",
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT || "", 10),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   synchronize: false,
   logging: false,
   entities: [
